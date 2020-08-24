0365ce6cc821ccc70d0f51d2c4216256c9377429
Lutz Ballaschke <python@tastybyte.de>
2020-08-24 03:56:15 +0200

    down to the bottom cleanup

--- play.py
+++ play.py
@@ -3,8 +3,15 @@ import numpy as np
 from libwave import *
 
 if __name__ == '__main__':
-    a1, a2 = awesome_bomb(31075)
-    #b1, b2 = awesome_bomb(31075)
+    duration = 31075
+    a1 = a2 = np.zeros(1)
+    x = 50
+
+    b1, b2 = awesome_bomb(duration)
+    for freq in (6 * x, 5 * x, 4 * x, 3 * x, 4 * x, 5 * x):
+        a1 = np.concatenate((a1, triangular_sample(freq, duration*4, rate)))
+    for freq in (6 * x, 5 * x, 4 * x, 3 * x, 4 * x, 5 * x):
+        a1 = np.concatenate((a1, triangular_sample(freq*2, duration*4, rate)))
     print('initial len %d' % len(a1))
     if len(a1) < len(a2):
         diff = len(a2) - len(a1)
