cd36e365490be5aae48303412161f48e7554c370
Lutz <python@tastybyte.de>
2021-05-16 11:53:46 +0200

    display font

--- animation.py
+++ animation.py
@@ -13,11 +13,16 @@ def _draw_lines(screen, vals, rgb, width, height):
 
     vtup = zip(list(range(width)), vals[:width])
     for _xy in vtup:
-        xy = (_xy[0], _xy[1] * (1/(2*POS_MAX/height)) + height/2) # scale amplitude to graph-size
+        xy = (_xy[0], _xy[1] * (1/(2*POS_MAX/height)) * 0.8 + height/2) # scale amplitude to graph-size
         if last_spot is not None:
             pygame.draw.line(screen, line_color, last_spot, xy, 2)
             print('({}:{} -> {}:{})'.format(last_spot[0], last_spot[1], xy[0], xy[1]))
         last_spot = xy
+
+def controls_display():
+    pygame.font.init()
+    controls_font = pygame.font.SysFont('dejavusans', 24)
+    return controls_font.render('frequency', True, (55,200,200))
 
 def play_wave(audio):
     pygame.mixer.init()
@@ -41,9 +46,13 @@ def draw_wave(f1, f2):
 
     background.fill((20, 20, 20))
     screen.blit(background, (0, 0))
+
     _draw_coordinate_cross(screen, width, height)
     _draw_lines(screen, f1, [55, 155, 55], width, height)
     _draw_lines(screen, f2, [166, 66, 66], width, height)
+
+    controls = controls_display()
+    screen.blit(controls, (800, 20))
 
     pygame.display.flip()
 
